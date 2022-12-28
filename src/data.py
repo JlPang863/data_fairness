0218c95cc3ac2be5426958c4bd832962315ea0b0
Zhaowei Zhu <zwzhu1995@gmail.com>
2022-12-28 15:55:50 -0800

    Update data.py

--- src/data.py
+++ src/data.py
@@ -130,12 +130,12 @@ def load_celeba_dataset_torch(args, shuffle_files=False, split='train', batch_si
 
 
   dataloader_1 = torch.utils.data.DataLoader(ds_1,
-                                            batch_size=batch_size if split == 'train' else 64, # val loader: 32
+                                            batch_size=batch_size if split == 'train' else 256, # val loader: 256
                                             shuffle=shuffle_files,
                                             num_workers=4,
                                             drop_last=False)
   dataloader_2 = torch.utils.data.DataLoader(ds_2,
-                                          batch_size=batch_size if split == 'test' else 64, # unlabeled loader: 32
+                                          batch_size=batch_size if split == 'test' else 64, # unlabeled loader: 64
                                           shuffle=shuffle_files,
                                           num_workers=4,
                                           drop_last=False)
