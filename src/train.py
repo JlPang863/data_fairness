4a3bb936a330909280b9a8872bd02681a94f0ec4
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-04 15:24:42 -0800

    for test

--- src/train.py
+++ src/train.py
@@ -194,7 +194,7 @@ def sample_by_infl(args, state, val_data, unlabeled_data, num):
 
     idx += batch['index'].tolist()
     # print(len(score))
-    if len(score) >= num * 100:
+    if len(score) >= num * 1: # 100
       break
 
 
