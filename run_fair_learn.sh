1ad34644b0df44ae105cfb832013ad9ffdf0f25c
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-11 20:15:26 -0800

    Update run_fair_learn.sh

--- run_fair_learn.sh
+++ run_fair_learn.sh
@@ -34,6 +34,7 @@
 
 # done
 
+SR="25"
 
     CUDA_VISIBLE_DEVICES=0 nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf entropy  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy 2 --sel_round $SR  > ./logs/fair_train/s2_dp_02_new256_100round_sel_$SR\_case1_remove_unfair.log &
 
