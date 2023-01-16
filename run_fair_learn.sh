4265cc125c97fe019720f79e14fa5a9b27197cc4
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-16 15:04:45 -0800

    Update run_fair_learn.sh

--- run_fair_learn.sh
+++ run_fair_learn.sh
@@ -65,60 +65,60 @@
 
 # SR="25"
 
-conf="entropy peer"
-sel_round="15 20 25"
-exp="1 2 3"
-strategy="2"
-conf_method="TV V"
+# conf="entropy peer"
+# sel_round="15 20 25"
+# exp="1 2 3"
+# strategy="2"
+# conf_method="TV V"
 
 
 
 
-i=0
-j=0
-for MYEXP in $exp
-do
+# i=0
+# j=0
+# for MYEXP in $exp
+# do
 
-for SR in $sel_round
-do
+# for SR in $sel_round
+# do
 
 
-for MYCONF in $conf
-do
+# for MYCONF in $conf
+# do
 
-    for CM in $conf_method
-    do
-        for STG in $strategy
-        do
-            echo GPU: $i. Task: $j. Rrunning for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
-            CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
+#     for CM in $conf_method
+#     do
+#         for STG in $strategy
+#         do
+#             echo GPU: $i. Task: $j. Rrunning for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
+#             CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
             
-            j=$((j+1))
-            if [[ $j -eq 2 ]]
-            then
-                i=$((i+1))
-                j=0
-            fi
-            if [[ $i -eq 2 ]]
-            then
-                i=0
-                echo wait
-                wait
-            fi
+#             j=$((j+1))
+#             if [[ $j -eq 2 ]]
+#             then
+#                 i=$((i+1))
+#                 j=0
+#             fi
+#             if [[ $i -eq 2 ]]
+#             then
+#                 i=0
+#                 echo wait
+#                 wait
+#             fi
             
-        done
-    done
+#         done
+#     done
 
 
-done
+# done
 
-done
-done
+# done
+# done
 
 
 
 conf="no_conf"
-sel_round="5 10 15 20 25"
+sel_round="5 10"
 exp="1 2 3"
 strategy="2"
 conf_method="TV"
@@ -126,8 +126,8 @@ conf_method="TV"
 
 
 
-# i=0
-# j=0
+i=0
+j=0
 for MYEXP in $exp
 do
 
