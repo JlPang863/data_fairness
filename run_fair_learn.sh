8d995d8ce706984fa40b3db6bfe33b979ae14da0
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-15 21:13:12 -0800

    Update run_fair_learn.sh

--- run_fair_learn.sh
+++ run_fair_learn.sh
@@ -137,7 +137,7 @@ do
             echo GPU: $i running for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
             # CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
             j=$((j+1))
-            if [[ $j -eq 1 ]]
+            if [[ $j -eq 2 ]]
             then
                 i=$((i+1))
                 j=0
