d82f35f6a9d245345a2caae168107b4b186f9906
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-15 20:48:33 -0800

    Update run_fair_learn.sh

--- run_fair_learn.sh
+++ run_fair_learn.sh
@@ -65,45 +65,45 @@
 
 # SR="25"
 
-conf="entropy peer"
-sel_round="5 10"
-exp="1 2 3"
-strategy="2"
-conf_method="TV V"
+# conf="entropy peer"
+# sel_round="5 10"
+# exp="1 2 3"
+# strategy="2"
+# conf_method="TV V"
 
 
 
 
-for MYEXP in $exp
-do
+# for MYEXP in $exp
+# do
 
 
 
-for SR in $sel_round
-do
+# for SR in $sel_round
+# do
 
-i=0
+# i=0
 
-for MYCONF in $conf
-do
+# for MYCONF in $conf
+# do
 
-    for CM in $conf_method
-    do
-        for STG in $strategy
-        do
-            echo running for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
-            CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
+#     for CM in $conf_method
+#     do
+#         for STG in $strategy
+#         do
+#             echo running for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
+#             CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
             
-        done
-    done
+#         done
+#     done
 
-i=$((i+1))
-# echo $i
-done
-wait 
+# i=$((i+1))
+# # echo $i
+# done
+# wait 
 
-done
-done
+# done
+# done
 
 
 
@@ -124,7 +124,7 @@ do
 
 
 i=0
-
+j=0
 for SR in $sel_round
 do
 for MYCONF in $conf
@@ -134,8 +134,19 @@ do
     do
         for STG in $strategy
         do
-            echo running for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
-            CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
+            echo GPU: $i running for ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log
+            # CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba_fair_learn.py --method dynamic_lmd  --lmd 0.0 --mu 1.0  --warm_epoch -1 --conf $MYCONF  --metric dp --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_round $SR --remove_pos --exp $MYEXP --conf_method $CM > ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log &
+            j=$((j+1))
+            if [[ $j -eq 2 ]]
+            then
+                i=$((i+1))
+                j=0
+            fi
+            if [[ $i -eq 2 ]]
+            then
+                i=0
+                wait
+            fi
             
         done
     done
