ee46af25ade1789a02fc0a835de94cc75e31115f
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-17 14:39:42 -0800

    Update get_result.sh

--- get_result.sh
+++ get_result.sh
@@ -30,7 +30,7 @@ do
         for STG in $strategy
         do
             echo $SR\_$MYCONF\_exp$MYEXP\_$CM
-            cat ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
+            cat ./logs/fair_train/s$STG\_dp_02_new256_100round_warm1000_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
             
         done
     done
@@ -43,3 +43,47 @@ wait
 done
 done
 
+
+conf="no_conf"
+sel_round="5 10 15 20 25"
+exp="1 2 3"
+strategy="2"
+conf_method="TV V"
+
+# conf="no_conf"
+# sel_round="5 10 15 20 25"
+# exp="1 2 3"
+# strategy="2"
+# conf_method="TV"
+
+
+for MYEXP in $exp
+do
+
+
+
+for SR in $sel_round
+do
+
+i=0
+
+for MYCONF in $conf
+do
+
+    for CM in $conf_method
+    do
+        for STG in $strategy
+        do
+            echo $SR\_$MYCONF\_exp$MYEXP\_$CM
+            cat ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
+            
+        done
+    done
+
+i=$((i+1))
+
+done
+wait 
+
+done
+done
