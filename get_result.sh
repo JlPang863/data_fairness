fff98953fa546296231eb6cf16b5ceb74daa4b6f
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-01-31 20:55:36 -0800

    Update get_result.sh

--- get_result.sh
+++ get_result.sh
@@ -5,12 +5,12 @@
 # strategy="2"
 # conf_method="TV V"
 
-conf="entropy peer"
-# sel_round="5 10"
-sel_round="5"
-exp="1 2 3"
-strategy="6"
-conf_method="TV V"
+# conf="entropy peer"
+# # sel_round="5 10"
+# sel_round="5"
+# exp="1 2 3"
+# strategy="6"
+# conf_method="TV V"
 
 
 # conf="no_conf"
@@ -18,15 +18,23 @@ conf_method="TV V"
 # exp="1 2 3"
 # strategy="2"
 # conf_method="TV"
+conf="no_conf"
+sel_round="5"
+val_ratio="0.2 0.4 0.6 0.8"
+exp="1 2 3"
+strategy="6"
+conf_method="TV"
 
+for VR in $val_ratio
+do
 
 for MYEXP in $exp
 do
 
 
 
-for SR in $sel_round
-do
+# for SR in $sel_round
+# do
 
 i=0
 
@@ -40,7 +48,11 @@ do
             echo $SR\_$MYCONF\_exp$MYEXP\_$CM
             # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_warm1000_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
             # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_warm2500_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
-            cat ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "98.79" | grep "test" | awk '{ print $19, $26 }'
+            # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "98.79" | grep "test" | awk '{ print $19, $26 }'
+
+            cat ./logs/fair_train/s$STG\_dp_02_new256_100round_val_$VR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "98.79" | grep "test" | awk '{ print $19, $26 }'
+
+            
 
             
             
