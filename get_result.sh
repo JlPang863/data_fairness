7d84ca04af46120435d548b80fa371a39594b223
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-01 18:36:27 -0800

    Update get_result.sh

--- get_result.sh
+++ get_result.sh
@@ -63,7 +63,7 @@ do
     do
         for STG in $strategy
         do
-            echo $VR\_$MYCONF\_exp$MYEXP\_$CM
+            echo $VR\_$TR\_$MYCONF\_exp$MYEXP\_$CM
             # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_warm1000_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
             # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_warm2500_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "95.15" | grep "test" | awk '{ print $20, $27 }'
             # cat ./logs/fair_train/s$STG\_dp_02_new256_100round_sel_$SR\_$MYCONF\_exp$MYEXP\_$CM.log | grep "98.79" | grep "test" | awk '{ print $19, $26 }'
