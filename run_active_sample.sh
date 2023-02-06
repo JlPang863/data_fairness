bcb03f9251a10d8c975c46fab81464011b77f344
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-05 18:11:50 -0800

    Update run_active_sample.sh

--- run_active_sample.sh
+++ run_active_sample.sh
@@ -5,7 +5,7 @@
 
 
 sel_layers="2 4"
-strategy="1 2 5"
+strategy="2 5"
 label_key="Smiling Straight_Hair Attractive"
 metric="dp eop eod"
 
@@ -13,6 +13,49 @@ metric="dp eop eod"
 
 i=0
 j=0
+
+for LABEL in $label_key
+do
+
+for LAYER in $sel_layers
+do
+
+for STG in $strategy
+do
+
+for MTC in $metric
+do
+
+echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
+
+CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 0  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL > ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log & 
+
+j=$((j+1))
+if [[ $j -eq 2 ]]
+then
+    i=$((i+1))
+    j=0
+fi
+if [[ $i -eq 8 ]]
+then
+    i=0
+    echo wait
+    wait
+fi
+
+done
+done
+done
+done
+
+
+
+sel_layers="2"
+strategy="1"
+label_key="Smiling Straight_Hair Attractive"
+metric="dp eop eod"
+
+
 
 for LABEL in $label_key
 do
