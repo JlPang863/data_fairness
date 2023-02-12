c5638df067c880e33cda0cddaaf8714581e4d8e0
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-11 21:43:26 -0800

    1

--- src/data.py
+++ src/data.py
@@ -329,7 +329,7 @@ def load_compas_dataset_torch(args, shuffle_files=False, split='train', batch_si
                                               batch_size=batch_size if split == 'train' else 512, # val loader: 512
                                               shuffle=shuffle_files,
                                               num_workers=1,
-                                              drop_last=True)
+                                              drop_last=False)
     dataloader_2 = torch.utils.data.DataLoader(ds_2,
                                             batch_size=batch_size if split == 'test' else 32, # unlabeled loader: 32
                                             shuffle=shuffle_files,
