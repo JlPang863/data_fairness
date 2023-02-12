85bf43e9ca9d1bf55b938d2ef2c1ec818be9e5c4
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-11 21:21:57 -0800

    Update run_active_sampling_compas.sh

--- run_active_sampling_compas.sh
+++ run_active_sampling_compas.sh
@@ -8,36 +8,36 @@ j=0
 
 
 
-for LAYER in $sel_layers
-do
+# for LAYER in $sel_layers
+# do
 
-for STG in $strategy
-do
+# for STG in $strategy
+# do
 
-for MTC in $metric
-do
+# for MTC in $metric
+# do
 
-# echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
+# # echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
 
-CUDA_VISIBLE_DEVICES=-1 nohup python3 run_compas.py --metric $MTC --label_ratio 0.05 --val_ratio 0.2 --strategy $STG --sel_layers $LAYER --warm_epoch 5 > ./logs/fair_sampling/compas/label_s$STG\_$MTC\_$LAYER.log & 
+# CUDA_VISIBLE_DEVICES=-1 nohup python3 run_compas.py --metric $MTC --label_ratio 0.05 --val_ratio 0.2 --strategy $STG --sel_layers $LAYER --warm_epoch 5 > ./logs/fair_sampling/compas/label_s$STG\_$MTC\_$LAYER.log & 
 
-j=$((j+1))
-if [[ $j -eq 2 ]]
-then
-    i=$((i+1))
-    j=0
-fi
-if [[ $i -eq 8 ]]
-then
-    i=0
-    echo wait
-    wait
-fi
+# j=$((j+1))
+# if [[ $j -eq 2 ]]
+# then
+#     i=$((i+1))
+#     j=0
+# fi
+# if [[ $i -eq 8 ]]
+# then
+#     i=0
+#     echo wait
+#     wait
+# fi
 
-done
+# done
 
-done
-done
+# done
+# done
 
 
 sel_layers="4"
