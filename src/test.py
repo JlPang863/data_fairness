d15deb7ed6713886b44748e87dfa8d80999d08c5
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-20 15:35:22 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -55,6 +55,27 @@
 import torchvision
 import torchvision.transforms as transforms
 import torch
+
+from typing import Any, Tuple
+class my_imagenet(torchvision.datasets.ImageNet):
+      def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target, index
+
+
 train_transform = transforms.Compose([
     transforms.Resize(32),
     # transforms.RandomCrop(32, padding=4), 
@@ -63,8 +84,11 @@ train_transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
-ds_new = torchvision.datasets.ImageNet(root = '/data2/data/imgnet/', split='train', transform=train_transform,
+ds_new = my_imagenet(root = '/data2/data/imgnet/', split='train', transform=train_transform,
                                      target_transform=None)
+
+# ds_new = torchvision.datasets.ImageNet(root = '/data2/data/imgnet/', split='train', transform=train_transform,
+                                    #  target_transform=None)
 import pdb
 pdb.set_trace()
 
