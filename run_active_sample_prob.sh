bb3e27e988cb5f85217d73d151e76ebd5ec98b09
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-20 19:23:51 -0800

    Update run_active_sample_prob.sh

--- run_active_sample_prob.sh
+++ run_active_sample_prob.sh
@@ -1,11 +1,11 @@
 
 
 sel_layers="4"
-strategy="2 5"
+strategy="5"
 label_key="Smiling Attractive Young Big_Nose"
 metric="dp eop eod"
 TOL="0.02"
-prob_all="0.1 0.3 0.5 0.7 0.9"
+prob_all="0.9 0.95 0.99"
 
 i=0
 j=0
@@ -27,7 +27,7 @@ do
 
 echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
 
-CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB.log & 
+CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB\_warm2batch.log & 
 
 j=$((j+1))
 if [[ $j -eq 3 ]]
