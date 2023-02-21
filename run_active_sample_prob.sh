5345c26dc6808c10f6b8d893caba304bdd9de540
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-20 22:44:52 -0800

    Update run_active_sample_prob.sh

--- run_active_sample_prob.sh
+++ run_active_sample_prob.sh
@@ -35,9 +35,9 @@ then
     i=$((i+1))
     j=0
 fi
-if [[ $i -eq 8 ]]
+if [[ $i -eq 5 ]]
 then
-    i=1
+    i=0
     echo wait
     wait
 fi
@@ -48,3 +48,41 @@ done
 done
 done
 
+
+# for LABEL in $label_key
+# do
+
+# for LAYER in $sel_layers
+# do
+
+# for STG in $strategy
+# do
+
+# for MTC in $metric
+# do
+
+# for PROB in $prob_all
+# do
+
+# echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
+
+# CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB --aux_data imagenet  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB\_warm2batch_imgaux.log & 
+
+# j=$((j+1))
+# if [[ $j -eq 3 ]]
+# then
+#     i=$((i+1))
+#     j=0
+# fi
+# if [[ $i -eq 8 ]]
+# then
+#     i=1
+#     echo wait
+#     wait
+# fi
+
+# done
+# done
+# done
+# done
+# done
