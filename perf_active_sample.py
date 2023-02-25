6db85bd13298d4606fda0d6f234f9e1b4487cceb
Zhaowei Zhu <zwzhu1995@gmail.com>
2023-02-25 15:40:40 -0800

    half ablation

--- perf_active_sample.py
+++ perf_active_sample.py
@@ -16,7 +16,8 @@ tol = 0.01
 avg_cnt = 3
 # suffix = '_tol0.02_wolb'
 # suffix = '_prob_0.9'
-suffix = '_prob_0.9_warm2batch_imgaux'
+suffix = '_prob_0.9_warm2batch'
+# suffix = '_prob_0.9_warm2batch_imgaux'
 
 
 root = f'./logs/fair_sampling/{dataset}/'
@@ -160,7 +161,8 @@ sel_layers = [4]
 strategy = [0, 5]
 if dataset == 'celeba':
     # label_key = ['Smiling', 'Straight_Hair', 'Attractive', 'Pale_Skin', 'Young', 'Big_Nose']
-    label_key = ['Smiling', 'Attractive', 'Big_Nose']
+    # label_key = ['Smiling', 'Attractive', 'Big_Nose']
+    label_key = ['Young']
 elif dataset == 'compas':
     label_key = ['label']
 else:
--- run_active_sample_prob.sh
+++ run_active_sample_prob.sh
@@ -5,7 +5,8 @@ strategy="5"
 label_key="Smiling Attractive Young Big_Nose"
 metric="dp eop eod"
 TOL="0.02"
-prob_all="0.9 0.95 0.99"
+# prob_all="0.9 0.95 0.99"
+prob_all="0.95"
 
 # i=0
 # j=0
@@ -48,7 +49,7 @@ prob_all="0.9 0.95 0.99"
 # done
 # done
 
-i=5
+i=0
 j=0
 for LABEL in $label_key
 do
@@ -67,7 +68,9 @@ do
 
 echo GPU: $i. Task: $j. Rrunning for ./logs/fair_sampling/$LABEL\_s$STG\_$MTC\_$LAYER.log
 
-CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB --aux_data imagenet  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB\_warm2batch_imgaux.log & 
+# CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB --aux_data imagenet  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB\_warm2batch_imgaux.log & 
+
+CUDA_VISIBLE_DEVICES=$i nohup python3 run_celeba.py --method plain  --warm_epoch 2  --metric $MTC --label_ratio 0.02 --val_ratio 0.1 --strategy $STG --sel_layers $LAYER --label_key $LABEL --new_prob $PROB --half_ablation  > ./logs/fair_sampling/celeba/$LABEL\_s$STG\_$MTC\_$LAYER\_prob_$PROB\_warm2batch_half_ablation.log & 
 
 j=$((j+1))
 if [[ $j -eq 3 ]]
@@ -77,7 +80,7 @@ then
 fi
 if [[ $i -eq 7 ]]
 then
-    i=5
+    i=0
     echo wait
     wait
 fi
